d44960dc807e64a0c0e631bdad7047781f44e5f9
zthking <tzhang017@gmail.com>
2019-06-02 16:20:53 -0400

    Revised generics issue

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 /**
  * Implement BoundedQueue<T>
  */
-public class ArrayRingBuffer<T> extends AbstractBoundedQueue implements BoundedQueue<T>{
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> implements BoundedQueue<T>{
     /** Index for the next dequeue or peek. */
     private int first;
     /** Index for the next enqueue. */
