60eff89cd07dad2d75956999c8b99bd268766560
zthking <tzhang017@gmail.com>
2019-06-02 15:48:36 -0400

    Revised

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
  * and can only be dequeued from the front of the queue.
  * It has a fixed capacity, and nothing is allowed to enqueue if the queue is full.
  */
-public abstract class AbstractBoundedQueue<T>{
+public abstract class AbstractBoundedQueue<T> extends BoundedQueue{
 
     protected int fillCount;
     protected int capacity;
