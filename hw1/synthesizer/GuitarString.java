ea004322e47c7be9124070023630e0cddd2b880f
zthking <tzhang017@gmail.com>
2019-06-02 15:21:30 -0400

    Add abstract

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -10,7 +10,7 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    private BoundedQueue<Double> buffer;
+    private AbstractBoundedQueue<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
